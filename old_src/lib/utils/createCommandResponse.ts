407331fc2a5e373b874a75548a6caa6de6193405
Jeroen Claassens <support@favware.tech>
2021-07-04 02:31:24 +0200

    chore: s/import/include/

--- old_src/lib/utils/createCommandResponse.ts
+++
@@ -1,6 +0,0 @@
-import type { APIInteractionResponse } from 'discord-api-types/v8';
-import { createJSONResponse } from './createJSONResponse';
-
-export function createCommandResponse(data: APIInteractionResponse) {
-	return createJSONResponse(data);
-}

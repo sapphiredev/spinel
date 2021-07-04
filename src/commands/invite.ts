645e9df88ff8321ff1905c10a20a513354c52bdc
Jeroen Claassens <support@favware.tech>
2021-07-04 23:00:42 +0200

    refactor: better invite response

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -1,10 +1,14 @@
+import { bold, hyperlink, hideLinkEmbed } from '@discordjs/builders';
 import type { APIInteractionResponse } from 'discord-api-types';
 import { DiscordApplicationId } from '../lib/env';
 import { interactionResponse } from '../lib/responseHelpers';
 
 export function invite(): APIInteractionResponse {
 	return interactionResponse({
-		content: `Add the Sapphire interaction to your server: [(click here)](<https://discord.com/api/oauth2/authorize?client_id=${DiscordApplicationId}&scope=applications.commands>)`,
+		content: `Add the Sapphire interaction to your server: ${hyperlink(
+			bold('click here'),
+			hideLinkEmbed(`https://discord.com/api/oauth2/authorize?client_id=${DiscordApplicationId}&scope=applications.commands`)
+		)}`,
 		ephemeral: true
 	});
 }

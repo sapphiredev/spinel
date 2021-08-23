e5d0999e7007c7d33c7a518a29df2636da03a89d
Jeroen Claassens <support@favware.tech>
2021-08-23 17:27:19 +0200

    chore: use "as const"

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,10 @@
 import { RedCross } from './emotes';
 import { AlgoliaApplicationId } from './env';
 
-export const FailPrefix = `${RedCross} I am sorry, but`;
-export const AlgoliaUrl = `https://${AlgoliaApplicationId}.algolia.net/1/indexes/discordjs/query`;
-export const MdnUrl = `https://developer.mozilla.org`;
-export const NodeUrl = 'https://nodejs.org';
+export const FailPrefix = `${RedCross} I am sorry, but` as const;
+export const AlgoliaUrl = `https://${AlgoliaApplicationId}.algolia.net/1/indexes/discordjs/query` as const;
+export const MdnUrl = `https://developer.mozilla.org` as const;
+export const NodeUrl = 'https://nodejs.org' as const;
 
 export function cast<T>(value: unknown): T {
 	return value as T;

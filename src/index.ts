6092cc33ecee8f11f9a301279da5002d1c9bb37f
Jeroen Claassens <support@favware.tech>
2021-10-16 00:07:17 +0200

    chore: extra logging

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,9 @@ config({
 export default async (req: VercelRequest, res: VercelResponse): Promise<VercelResponse> => {
 	// Load up the tags into the cache
 	await loadTags();
+	console.log('Handling request!');
+	console.error('Handling request!');
+	console.warn('Handling request!');
 
 	const interactionInvalid = verifyDiscordInteraction(req);
 	if (interactionInvalid) {

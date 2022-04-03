e041b23d25b65e8ec764df06999171f086908036
Jeroen Claassens <support@favware.tech>
2022-04-04 01:41:53 +0200

    refactor: update to latest HFx

--- src/interaction-handlers/tag.ts
+++ src/interaction-handlers/tag.ts
@@ -2,11 +2,11 @@ import { SupportServerButton } from '#constants/constants';
 import { errorResponse } from '#utils/response-utils';
 import { findTag } from '#utils/tags';
 import { ActionRowBuilder, type MessageActionRowComponentBuilder } from '@discordjs/builders';
-import { MessageComponentHandler, postMessage } from '@skyra/http-framework';
+import { InteractionHandler, postMessage } from '@skyra/http-framework';
 import type { APIMessageComponentInteraction, APIMessageSelectMenuInteractionData, Snowflake } from 'discord-api-types/v10';
 
-export class UserMessageComponentHandler extends MessageComponentHandler {
-	public run(interaction: APIMessageComponentInteraction, [customIdValue]: [Snowflake | null]) {
+export class UserMessageComponentHandler extends InteractionHandler {
+	public *run(interaction: APIMessageComponentInteraction, [customIdValue]: [Snowflake | null]) {
 		const content = findTag((interaction.data as APIMessageSelectMenuInteractionData).values[0], customIdValue ?? undefined);
 
 		if (!content) {
@@ -18,16 +18,15 @@ export class UserMessageComponentHandler extends MessageComponentHandler {
 			);
 		}
 
-		return {
-			response: this.updateMessage({ content: 'Tag sent', components: [] }),
-			callback: () => {
-				void postMessage(interaction, {
-					content,
-					allowed_mentions: {
-						users: customIdValue ? [customIdValue] : []
-					}
-				});
+		yield this.updateMessage({ content: 'Tag sent', components: [] });
+
+		void postMessage(interaction, {
+			content,
+			allowed_mentions: {
+				users: customIdValue ? [customIdValue] : []
 			}
-		};
+		});
+
+		return undefined;
 	}
 }

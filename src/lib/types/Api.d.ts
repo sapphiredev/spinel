961ba19e5edaad0e863a73aa2db942790de91f61
Jeroen Claassens <support@favware.tech>
2022-04-04 01:07:45 +0200

    chore: remove unused types

--- src/lib/types/Api.d.ts
+++
@@ -1,5 +0,0 @@
-import type { FastifyReply } from 'fastify';
-import type { RouteGenericInterface } from 'fastify/types/route';
-import type { IncomingMessage, Server, ServerResponse } from 'http';
-
-export type FastifyResponse = FastifyReply<Server, IncomingMessage, ServerResponse, RouteGenericInterface, unknown>;

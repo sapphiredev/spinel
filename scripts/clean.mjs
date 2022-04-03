d455a909cbf1aec3b2b1d7706a4442c5a5f78d1b
Jeroen Claassens <support@favware.tech>
2022-04-03 18:51:55 +0200

    chore: update clean and tsconfig

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -1,8 +1,6 @@
 import { rm } from 'node:fs/promises';
 
-const apiDir = new URL('../api/', import.meta.url);
 const distDir = new URL('../dist/', import.meta.url);
 
 const config = { recursive: true, force: true };
-await rm(apiDir, config);
 await rm(distDir, config);

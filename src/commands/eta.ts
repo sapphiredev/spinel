9e2ef78fe118d7f2214c5343cec5c375160e24e7
Jeroen Claassens <support@favware.tech>
2022-08-20 22:41:39 +0200

    refactor(eta): remove v3 eta

--- src/commands/eta.ts
+++ src/commands/eta.ts
@@ -4,7 +4,6 @@ import { Command, RegisterCommand, RestrictGuildIds } from '@skyra/http-framewor
 import { addDays, addMonths } from 'date-fns';
 
 const enum Eta {
-	V3 = 'v3',
 	V4 = 'v4'
 }
 
@@ -16,27 +15,24 @@ const enum Eta {
 @RestrictGuildIds(getGuildIds())
 export class UserCommand extends Command {
 	public override async chatInputRun() {
-		const etaToV3 = await this.getEta(Eta.V3);
 		const etaToV4 = await this.getEta(Eta.V4);
 
-		const timeParsedEtaV3 = time(etaToV3, TimestampStyles.RelativeTime);
 		const timeParsedEtaV4 = time(etaToV4, TimestampStyles.RelativeTime);
 		const frameworkLink = hyperlink(inlineCode('@sapphire/framework'), hideLinkEmbed('https://github.com/sapphiredev/framework'));
 
-		const newEtaV3 = addDays(etaToV3, 1);
 		const newEtaV4 = addDays(etaToV4, 4);
 
-		await Promise.all([this.setEta(newEtaV3, Eta.V3), this.setEta(newEtaV4, Eta.V4)]);
+		await this.setEta(newEtaV4, Eta.V4);
 
 		return this.message({
-			content: `${frameworkLink} Version 3 will be releasing ${timeParsedEtaV3} and Version 4 will be releasing ${timeParsedEtaV4}`
+			content: `${frameworkLink} Version 4 will be releasing ${timeParsedEtaV4}`
 		});
 	}
 
 	private async getEta(eta: Eta): Promise<Date> {
 		const etaFromRedis = await this.container.redisClient.get(`eta-${eta}`);
 
-		return etaFromRedis ? new Date(Number(etaFromRedis)) : addMonths(new Date(), eta === Eta.V3 ? 1 : 4);
+		return etaFromRedis ? new Date(Number(etaFromRedis)) : addMonths(new Date(), 4);
 	}
 
 	private async setEta(newEta: Date, eta: Eta): Promise<void> {

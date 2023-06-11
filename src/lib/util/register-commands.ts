cfb34be9c8a84ee53733cec77932116f6dbe90ac
Jeroen Claassens <support@favware.tech>
2023-06-11 16:46:03 +0200

    refactor: use token for registering commands

--- src/lib/util/register-commands.ts
+++ src/lib/util/register-commands.ts
@@ -1,11 +1,8 @@
-import { getBearerToken } from '#utils/get-token';
 import { envParseString } from '@skyra/env-utilities';
 import { Registry } from '@skyra/http-framework';
 
 export async function registerCommands() {
-	const token = await getBearerToken();
-
-	const registry = new Registry({ token, authPrefix: 'Bearer' });
+	const registry = new Registry({});
 
 	if (envParseString('NODE_ENV') === 'development') {
 		await registry.registerGuildRestrictedCommands();

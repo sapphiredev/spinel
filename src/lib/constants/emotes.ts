6d9ea194b8103a6ec9f74222d9474a3c13152c64
Jeroen Claassens <support@favware.tech>
2023-12-10 15:50:31 +0100

    refactor: remove discordjs-docs search in favour of official bot

--- src/lib/constants/emotes.ts
+++ src/lib/constants/emotes.ts
@@ -1,8 +1,6 @@
 export const ExtractEmojiIdRegex = /<a?:_:(?<id>\d{17,18})>/;
 
 export const DjsGuideIcon = `<:_:814216203466965052>` as const;
-export const DjsDocsStableIcon = '<:_:851461487498493952>' as const;
-export const DjsDocsDevIcon = '<:_:851461195554619442>' as const;
 export const GreenTick = '<:_:637706251253317669>' as const;
 export const RedCross = '<:_:637706251257511973>' as const;
 export const MdnIcon = '<:_:861671077365284884>' as const;
@@ -15,17 +13,3 @@ export const GhPrMerged = '<:_:864240877416873984>' as const;
 export const GhPrOpen = '<:_:864240877479657482>' as const;
 export const DiscordDevelopersIcon = '<a:_:898636681920536576>' as const;
 export const SapphireGemId = '<:_:746069730170896405>' as const;
-
-// #region Discord Docs Emojis
-export const DiscordJsDocsClass = '<:_:898645160349552700>' as const;
-export const DiscordJsDocsClassDev = '<:_:898645160219512864>' as const;
-export const DiscordJsDocsEnumDev = '<:_:898645160257290240>' as const;
-export const DiscordJsDocsEnumOrInterface = '<:_:898645160219521134>' as const;
-export const DiscordJsDocsEvent = '<:_:898645159942713406>' as const;
-export const DiscordJsDocsEventDev = '<:_:898645160253087816>' as const;
-export const DiscordJsDocsField = '<:_:898645160286646322>' as const;
-export const DiscordJsDocsFieldDev = '<:_:898645160248897586>' as const;
-export const DiscordJsDocsInterfaceDev = '<:_:898645160336977960>' as const;
-export const DiscordJsDocsMethod = '<:_:898645159934316565>' as const;
-export const DiscordJsDocsMethodDev = '<:_:898645160244674560>' as const;
-// #endregion
--- src/lib/util/setup.ts
+++ src/lib/util/setup.ts
@@ -1,14 +1,9 @@
 import { RedisCacheClient } from '#lib/redis-cache/RedisCacheClient';
 import { loadTags } from '#utils/tags';
 import { setup } from '@skyra/env-utilities';
-import { Doc } from 'discordjs-docs-parser';
 import { URL } from 'node:url';
 
 process.env.NODE_ENV ??= 'development';
-
-Doc.setGlobalOptions({
-	escapeMarkdownLinks: true
-});
 
 setup(new URL('../../../.env', import.meta.url));
 
